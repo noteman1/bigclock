b6279b18ca9d9f71913d2ceeb91f7fd7dab1a1dd
noteman1 <noteman1@gmail.com>
2025-05-07 21:50:48 +0900

    Update: PWA

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'digital-clock-v1';
+const CACHE_FILES = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/icon-192x192.png',
+    '/icon-512x512.png'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(CACHE_FILES))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
